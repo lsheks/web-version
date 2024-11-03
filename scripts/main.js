a60bd8e8c2eba4f8e07cd3b681f64fcbac745f13
lsheks <38179007+lsheks@users.noreply.github.com>
2024-11-03 14:12:11 +0200

    make untap to clear

--- scripts/main.js
+++ scripts/main.js
@@ -31,13 +31,11 @@ $(function() {
             cy.style().fromJson(o.style).update();
 
             cy.on('tap', 'node', function(event) {
-                console.log('Event object:', event); // Log the event object
             
                 // Get the node from the event's cyTarget property
                 var node = event.cyTarget; // This should give you the node directly
             
                 if (node.length > 0 && node.isNode()) { // Ensure it's a valid node
-                    console.log('Tapped node:', node); // Log the tapped node
             
                     // Clear highlights from all nodes and edges
                     cy.nodes().removeClass('highlighted');
@@ -49,10 +47,20 @@ $(function() {
             
                     // Highlight neighbors of the tapped node
                     var neighbors = connectedEdges.connectedNodes();
-                    console.log('Neighbors:', neighbors); // Log neighbors for debugging
+                    //console.log('Neighbors:', neighbors); // Log neighbors for debugging
                     neighbors.addClass('highlighted');
                 } else {
                     console.error('Node is undefined or not a node');
+                }
+            });
+
+            cy.on('tap', function(event) {
+
+                // Check if the tap target is the background (cy itself)
+                if (!event.cyTarget.isNode && !event.cyTarget.isEdge) {        
+                    // Remove the 'highlighted' class from all nodes and edges
+                    cy.nodes().removeClass('highlighted');
+                    cy.edges().removeClass('highlighted');
                 }
             });
         }
